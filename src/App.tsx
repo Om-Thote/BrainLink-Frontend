045b1073672d5e357e5ad36bad016ed81575299b
Om Thote <omthote24@gmail.com>
2025-07-16 01:44:42 +0530

    Update API URLs for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { Signin } from "./pages/Signin"
 import { Signup } from "./pages/Signup"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
 import { Dashboard } from "./pages/dashboard"
 import { Landing } from "./pages/landing"
 
 
 function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Navigate to="/landing" replace />} />
-    <Route path="/landing" element={<Landing />} />
-    <Route path="/signup" element={<Signup />} />
-    <Route path="/signin" element={<Signin />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-  </Routes>
-  </BrowserRouter>
+  return <HashRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/landing" replace />} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </HashRouter>
 }
 
 export default App
